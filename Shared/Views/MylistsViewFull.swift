4d7199657f21428a72d8ec08a45a65e6f22d3a0a
hamza5645 <hamzaosama5645@outlook.com>
2022-08-26 08:08:50 +0200

    merged the homeViewModel and the homeViewModelFull making the code more efficient

--- Shared/Views/MylistsViewFull.swift
+++ Shared/Views/MylistsViewFull.swift
@@ -9,48 +9,86 @@ import SwiftUI
 
 struct MylistsViewFull: View {
     
+    @State private var isPresented: Bool = false
+    @StateObject private var vm:HomeViewModel
+    
+    init(vm:HomeViewModel) {
+        _vm = StateObject(wrappedValue: vm)
+    }
     let myLists: [MyListViewModel]
     
-    //    if myLists.isEmpty {
-    //        MyListsView()
-    //    } else {
-    //        List(myLists) { myList in
-    //            Text(myList.name)
-    //        }
-    //    }
+        if myLists.isEmpty {
+            NavigationView {
+                VStack {
+                    
+                    List(myLists) { myLists in
+                        Text(myLists.name)
+                    }
+    #if os(iOS)
+                    Divider()
+                    Button("Add List") {
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding()
+    #endif
+                    
+    #if os(macOS)
+                    Divider()
+                    Button("Add List") {
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding()
+    #endif
+                }
+            }
+            .navigationTitle("Progress Tracker")
+        } else {
+            NavigationView {
+                VStack {
+                    Button {
+                        isPresented = true
+                    } label: {
+                        VStack {
+                            Image(systemName: "plus")
+                                .font(.system(size: 128))
+                            //                        .frame(width:500,height:500,alignment:.center)
+                            Text("Create your first list")
+                        }
+                        .foregroundColor(.primary)
+                    }
+                    .buttonStyle(.plain)
+    #if os(iOS)
+                    .sheet(isPresented: $isPresented) {
+                        addNewListView { newListName, colorCode in
+                            //Save
+                            vm.saveNewList(newListName: newListName, colorCode: colorCode)
+                        }
+                    }
+    #endif
+                    
+    #if os(macOS)
+                    .sheet(isPresented: $isPresented) {
+                        addNewListView { newListName, colorCode in
+                            //Save
+                        }
+                    }
+    #endif
+                }
+                //        .frame(minWidth: 800, maxWidth: .infinity, minHeight: 1000,maxHeight: .infinity, alignment: .center)
+            }
+            .navigationTitle("Progress Tracker")
+            }
     
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                List(myLists) { myLists in
-                    Text(myLists.name)
-                }
-#if os(iOS)
-                Divider()
-                Button("Add List") {
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .padding()
-#endif
-                
-#if os(macOS)
-                Divider()
-                Button("Add List") {
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .padding()
-#endif
-            }
-        }
-        .navigationTitle("Progress Tracker")
+        Text("Hello")
     }
 }
 
 struct MylistsViewFull_Previews: PreviewProvider {
     static var previews: some View {
-        MylistsViewFull(myLists: [])
+        let vm = HomeViewModel()
+        MylistsViewFull(vm: vm)
     }
 }
